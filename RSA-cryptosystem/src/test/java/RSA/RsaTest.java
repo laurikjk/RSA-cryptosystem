da881c490112c540b32bb8de1ce6b9341db0b37e
LauriKajakko <kajakko.lauri@gmail.com>
2020-11-02 18:48:57 +0200

    added checkstyle

---
+++ RSA-cryptosystem/src/test/java/RSA/RsaTest.java
@@ -0,0 +1,48 @@
+
+package RSA;
+
+import RSA.domain.Encryption;
+import RSA.domain.KeyGenerator;
+import java.math.BigInteger;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author lkajakko
+ */
+public class RsaTest {
+    
+    KeyGenerator g;
+    Encryption e;
+    BigInteger priv;
+    BigInteger pub;
+    BigInteger n;
+    
+    @Before
+    public void setUp() {
+        g = new KeyGenerator(1024);
+        e = new Encryption();
+        priv = g.getPrivateKey();
+        pub = g.getPublicKey();
+        n = g.getn();
+    }
+    
+    @Test
+    public void messageAndDecryptedMessageMatch() {
+        BigInteger message = new BigInteger("1234567890");
+        BigInteger encrypted = e.encrypt(pub, message, n);
+        BigInteger decrypted = e.decrypt(priv, encrypted, n);
+        
+        assertTrue(message.equals(decrypted));
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
